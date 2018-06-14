66d7122c699246e3ca06f6e02315edeec0fec5c0
João Roda <joao.roda@netcabo.pt>
2018-06-14 11:38:08 +0100

    Added comprarbilhete.php

---
+++ comprarbilhete.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <style>
+      body{
+        background-image: url(resources/b.jpg);
+        background-repeat: no-repeat;
+        background-size: cover;
+        font-family: Verdana;
+        font-size: 20px;
+        margin: 0;
+        line-height: 24px;
+      }
+      .menu{
+        text-align: center;
+        width: 100%;
+        background: blue;
+        opacity: 0.7;
+      }
+      .menu ul{
+        margin: 0;
+        padding: 0;
+        list-style: none;
+        position: relative;
+      }
+      .menu ul li a{
+        display: block;
+        padding: 25px;
+        color: white;
+        text-decoration: none;
+        width: 150px;
+      }
+      .menu ul:after{
+        content: ""; clear: both;
+        display: block;
+      }
+      .menu ul li{
+        float: left;
+        list-style: none;
+      }
+      .menu ul ul{
+        display: none;
+      }
+      .menu ul li:hover > ul{
+        display: block;
+      }
+      .menu ul li:hover {
+        background: black;
+        transition: 0.9s;
+      }
+      .menu ul li:hover a{
+        color: white;
+      }
+      .menu ul ul{
+        background: black
+        padding: 0;
+        margin: 0;
+        position: absolute; top: 100%;
+      }
+      .menu ul ul li{
+        float: none;
+        position: relative;
+      }
+      .menu ul ul li a{
+        padding: 25px;
+        color: white;
+        width: 300px;
+        text-align: left;
+      }
+      .menu ul ul li a:hover{
+        background: lightblue;
+        color: black;
+        transition: 0.9s;
+      }
+    </style>
+//https://www.w3schools.com/howto/howto_js_dropdown.asp
+//https://www.daftlogic.com/information-programmatically-preselect-dropdown-using-javascript.htm
+
+  </head>
+  <body>
+
+    <form method="post" action="login.php">
+      <?php include('resources/errors.php'); ?>
+
+      <div class="dropdown">
+        <button onclick="myFunction()" class="dropbtn">Dropdown</button>
+        <div id="myDropdown" class="dropdown-content">
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+      </div>
+
+      <div class="input-group">
+        <label>Password</label>
+        <input type="password" name="password">
+      </div>
+      <div class="input-group">
+        <button type="submit" class="btn" name="login_user">Login</button>
+      </div>
+      <p>
+        Não tem conta? <a href="register.php">Registar</a> &ensp;
+        Página inicial <a href="inicio.php">voltar</a>
+      </p>
+    </form>
+
+  </body>
+</html>
