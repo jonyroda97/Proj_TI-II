01469f02d87bf1caee73225045199389970ba57a
João Roda <joao.roda@netcabo.pt>
2018-07-05 19:55:11 +0100

    Apresentação dos dados em info.php -- Falta css da tabela em info.php -- BD para a compra da viagem?

--- info.php
+++ info.php
@@ -1,80 +1,82 @@
+<?php
+session_start();
+?>
 <html>
 <head>
-<style>
-body{
-  background-image: url(resources/b.jpg);
-  background-repeat: no-repeat;
-  background-size: cover;
-  font-family: Verdana;
-  font-size: 20px;
-  margin: 0;
-  line-height: 24px;
-}
-.menu{
-  text-align: center;
-  width: 100%;
-  background: blue;
-  opacity: 0.7;
-}
-.menu ul{
-  margin: 0;
-  padding: 0;
-  list-style: none;
-  position: relative;
-}
-.menu ul li a{
-  display: block;
-  padding: 25px;
-  color: white;
-  text-decoration: none;
-  width: 150px;
-}
-.menu ul:after{
-  content: ""; clear: both;
-  display: block;
-}
-.menu ul li{
-  float: left;
-  list-style: none;
-}
-.menu ul ul{
-  display: none;
-}
-.menu ul li:hover > ul{
-  display: block;
-}
-.menu ul li:hover {
-  background: black;
-  transition: 0.9s;
-}
-.menu ul li:hover a{
-  color: white;
-}
-.menu ul ul{
-  background: black
-  padding: 0;
-  margin: 0;
-  position: absolute; top: 100%;
-}
-.menu ul ul li{
-  float: none;
-  position: relative;
-}
-.menu ul ul li a{
-  padding: 25px;
-  color: white;
-  width: 300px;
-  text-align: left;
-}
-.menu ul ul li a:hover{
-  background: lightblue;
-  color: black;
-  transition: 0.9s;
-}
- table{
-      margin-top: 8%;
-}
-}
+  <style>
+    body{
+      background-image: url(resources/b.jpg);
+      background-repeat: no-repeat;
+      background-size: cover;
+      font-family: Verdana;
+      font-size: 20px;
+      margin: 0;
+      line-height: 24px;
+    }
+    .menu{
+      text-align: center;
+      width: 100%;
+      background: blue;
+      opacity: 0.7;
+    }
+    .menu ul{
+      margin: 0;
+      padding: 0;
+      list-style: none;
+      position: relative;
+    }
+    .menu ul li a{
+      display: block;
+      padding: 25px;
+      color: white;
+      text-decoration: none;
+      width: 150px;
+    }
+    .menu ul:after{
+      content: ""; clear: both;
+      display: block;
+    }
+    .menu ul li{
+      float: left;
+      list-style: none;
+    }
+    .menu ul ul{
+      display: none;
+    }
+    .menu ul li:hover > ul{
+      display: block;
+    }
+    .menu ul li:hover {
+      background: black;
+      transition: 0.9s;
+    }
+    .menu ul li:hover a{
+      color: white;
+    }
+    .menu ul ul{
+      background: black
+      padding: 0;
+      margin: 0;
+      position: absolute; top: 100%;
+    }
+    .menu ul ul li{
+      float: none;
+      position: relative;
+    }
+    .menu ul ul li a{
+      padding: 25px;
+      color: white;
+      width: 300px;
+      text-align: left;
+    }
+    .menu ul ul li a:hover{
+      background: lightblue;
+      color: black;
+      transition: 0.9s;
+    }
+     table{
+          margin-top: 8%;
+    }
   </style>
 </head>
   <body>
@@ -98,44 +100,42 @@ body{
           </ul>
       </nav>
     </header>
-    <?php
-    include('resources/server.php');
-    $link = mysqli_connect("localhost", "root", "", "registration");
 
-    // Check connection
-    if($link === false){
-        die("ERROR: Could not connect. " . mysqli_connect_error());
-    }
-
-    // Attempt select query execution
-    $sql = "SELECT * FROM users WHERE  `username` = '".$_SESSION['username']."' ";
-    if($result = mysqli_query($link, $sql)){
-        if(mysqli_num_rows($result) > 0){
-            echo "<center><table>";
-                echo "<tr>";
-
-                    echo "<th>username</th>";
-                    echo "<th>email</th>";
-                echo "</tr>";
-            while($row = mysqli_fetch_array($result)){
-                echo "<tr>";
-                    
-                    echo "<td>" . $row['username'] . "</td>";
-                    echo "<td>" . $row['email'] . "</td>";
-                echo "</tr>";
-            }
-            echo "</center></table>";
-            // Free result set
-            mysqli_free_result($result);
-        } else{
-            echo "No records matching your query were found.";
-        }
-    } else{
-        echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-    }
-
-    // Close connection
-    mysqli_close($link);
-    ?>
-
+    <center>
+      <table border="2">
+        <tr>
+          <th>Username</th>
+          <td><?php echo $_SESSION['username'];?></td>
+        </tr>
+        <tr>
+          <th>Email</th>
+          <td><?php echo $_SESSION['email'];?></td>
+        </tr>
+        <tr>
+          <th>Pimeiro Nome</th>
+          <td><?php echo $_SESSION['prim'];?></td>
+        </tr>
+        <tr>
+          <th>Último Nome</th>
+          <td><?php echo $_SESSION['ult'];?></td>
+        </tr>
+        <tr>
+          <th>Número Telemóvel</th>
+          <td><?php echo $_SESSION['telm'];?></td>
+        </tr>
+        <tr>
+          <th>Morada</th>
+          <td><?php echo $_SESSION['morada'];?></td>
+        </tr>
+        <tr>
+          <th>Localidade</th>
+          <td><?php echo $_SESSION['local'];?></td>
+        </tr>
+        <tr>
+          <th>Código Postal</th>
+          <td><?php echo $_SESSION['zip'];?></td>
+        </tr>
+      </table>
+    </center>
+  </body>
 </html>
--- resources/server.php
+++ resources/server.php
@@ -1,15 +1,15 @@
 <?php
-session_start();
 
+session_start();
 
 $username = "";
 $email    = "";
 $errors = array();
 
-
 $db = mysqli_connect('localhost', 'root', '', 'registration');
 
 
+//Registar novo User
 if (isset($_POST['reg_user'])) {
 
   $username = mysqli_real_escape_string($db, $_POST['username']);
@@ -41,17 +41,18 @@ if (isset($_POST['reg_user'])) {
 
   if (count($errors) == 0) {
     $password = md5($password_1);
-
-    $query = "INSERT INTO users (username, email, password)
-          VALUES('$username', '$email', '$password')";
+    $query = "INSERT INTO users (username, email, password) VALUES('$username', '$email', '$password')";
     mysqli_query($db, $query);
     $_SESSION['username'] = $username;
+    $_SESSION['email'] = $email;
     $_SESSION['success'] = "Esta conectado";
     header('location: form.php');
   }
 }
 
+//Login
 if (isset($_POST['login_user'])) {
+
   $username = mysqli_real_escape_string($db, $_POST['username']);
   $password = mysqli_real_escape_string($db, $_POST['password']);
 
@@ -67,16 +68,37 @@ if (isset($_POST['login_user'])) {
     $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
     $results = mysqli_query($db, $query);
     if (mysqli_num_rows($results) == 1) {
+      $row = mysqli_fetch_array($results);
       $_SESSION['username'] = $username;
+      $_SESSION['id'] = $row['id'];
       $_SESSION['success'] = "Esta conectado";
-      header('location: inicio2.php');
-    }else {
+      $data_query = "SELECT * FROM users INNER JOIN dados ON users.id = dados.id WHERE users.id='".$_SESSION['id']."'";
+      $data_results = mysqli_query($db, $data_query);
+      if (mysqli_num_rows($data_results) == 1){
+        $data_row = mysqli_fetch_array($data_results);
+        $_SESSION['email'] = $data_row['email'];
+        $_SESSION['prim'] = $data_row['prim'];
+        $_SESSION['ult'] = $data_row['ult'];
+        $_SESSION['telm'] = $data_row['telm'];
+        $_SESSION['morada'] = $data_row['morada'];
+        $_SESSION['local'] = $data_row['local'];
+        $_SESSION['zip'] = $data_row['zip'];
+        header('location: inicio2.php');
+      } else {
+        array_push($errors, "Mais do que 1 resultado");
+      }
+    } else {
       array_push($errors, "Username/Password incorreto");
     }
+    mysqli_free_result($results);
+    mysqli_free_result($data_results);
   }
 }
 
+//Formulário
 if(isset($_POST['sub'])){
+  $username = $_SESSION['username'];
+
   $prim = mysqli_real_escape_string($db, $_POST['prim']);
   $ult = mysqli_real_escape_string($db, $_POST['ult']);
   $telm = mysqli_real_escape_string($db, $_POST['telm']);
@@ -91,20 +113,29 @@ if(isset($_POST['sub'])){
   if (empty($local)) { array_push($errors, "Localidade tem de estar preenchida"); }
   if (empty($zip)) { array_push($errors, "Código Postal tem de estar preenchida"); }
 
-  if (count($errors) == 0) {
-    $query = "INSERT INTO dados (prim, ult, telm, morada, local, zip)
-    VALUES('$prim', '$ult', '$telm', '$morada', '$local', '$zip')";
-    mysqli_query($db, $query);
-    header('location: inicio2.php');
+  $query = "SELECT * FROM users WHERE username='$username'";
+  $id_results = mysqli_query($db, $query);
+  if (mysqli_num_rows($id_results) == 1) {
+      $row = mysqli_fetch_array($id_results);
+      $_SESSION['id'] = $row['id'];
+  }else{
+    array_push($errors, "Não foi possível encontrar o utilizador");
   }
 
-  $_SESSION['prim'] = $prim;
-  $_SESSION['ult'] = $ult;
-  $_SESSION['telm'] = $telm;
-  $_SESSION['morada'] = $morada;
-  $_SESSION['local'] = $local;
-  $_SESSION['zip'] = $zip;
-
+  if (count($errors) == 0) {
+    $id = $_SESSION['id'];
+    $query = "INSERT INTO dados (prim, ult, telm, morada, local, zip, id)
+    VALUES('$prim', '$ult', '$telm', '$morada', '$local', '$zip', '$id')";
+    mysqli_query($db, $query);
+    unset($id);
+    $_SESSION['prim'] = $prim;
+    $_SESSION['ult'] = $ult;
+    $_SESSION['telm'] = $telm;
+    $_SESSION['morada'] = $morada;
+    $_SESSION['local'] = $local;
+    $_SESSION['zip'] = $zip;
+    header('location: inicio2.php');
+  }
 }
 
 ?>
